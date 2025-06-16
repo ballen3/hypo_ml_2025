1f99ca2da1cce98203b8bdd3619dd7f8e3c71d70
Brooke Allen <allenmbrooke@gmail.com>
2025-06-16 12:38:44 -0500

    Add descriptive comments and edited error messages for clarity

--- tests/04_ips_faa_clean.sh
+++ tests/04_ips_faa_clean.sh
@@ -4,24 +4,29 @@
 # Usage: 04_ips_faa_clean.sh /path/to/file.faa
 # Outputs: cleaned .faa file path (in temp dir)
 
+## Shouldn't have to change any of these at this level.
 INPUT="$1"
 TEMP_DIR="${2:-/tmp}"
 BASENAME=$(basename "$INPUT" .faa)
 
+## makes the temp_dir if it doesn't already exist (redundant but whatever)
 mkdir -p "$TEMP_DIR"
 
-
+## Error if an input isn't found
 if [[ ! -f "$INPUT" ]]; then
-    echo "Error: file at $INPUT not found." >&2
+    echo "Cleaning Error: file at $INPUT not found." >&2
     exit 1
 fi
 
-CLEANED="${TEMP_DIR}/${BASENAME}_cleaned.faa"
-
+## Remove asterisks (*) from protein sequences and write cleaned file to temp directory
+CLEANED="${TEMP_DIR}/${BASENAME}_clean"
 sed 's/\*//g' "$INPUT" > "$CLEANED"
 
+## Error if the cleaned file is not created
 if [[ ! -f "$CLEANED" ]]; then
-    echo "Failed to clean $BASENAME" >&2
+    echo "Cleaning Error: Failed to clean * from $BASENAME" >&2
     exit 2
 fi
+
+## Print the cleaned file path for downstream scripts to capture and use as input
 echo "$CLEANED"
